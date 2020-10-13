0c4bc2481f6412b159abf82ec7ea6742fbea18a7
asselkhassen <aselkhasen@gmail.com>
2020-10-13 22:48:20 +0600

    Lesson 1 - Homework

--- les1_task1_hw.py
+++ les1_task1_hw.py
@@ -5,9 +5,14 @@ username = 'asselkhassen'
 response = requests.get(f'https://api.github.com/users/{username}/repos')
 
 j_data = response.json()
+my_repos = []
+for el in j_data:
+    my_repos.append(el['name'])
 
 with open('task-1.json', 'w') as f:
-    json.dump(j_data, f)
+    json.dump(my_repos, f)
 
 for el in j_data:
     print(el['name'])
+
+
